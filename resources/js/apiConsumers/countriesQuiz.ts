5ff5a56eca807d37bf7fb88f7d7e64bd943a7371
Tom Rosenfeld <tomcodebass@gmail.com>
2025-04-15 16:08:08 +0100

    feat(capitalise build): Added postQuizAnswer api consumer.

--- resources/js/apiConsumers/countriesQuiz.ts
+++ resources/js/apiConsumers/countriesQuiz.ts
@@ -1,13 +1,36 @@
 import axios from 'axios';
 
+type TCountry = string;
+export type TCity = string;
+
 export interface IQuizData {
-    country: string;
-    cities: string[];
+    country: TCountry;
+    cities: TCity[];
 }
 
-export const getQuizDataRequest = async () => {
+export interface IQuizAnswer {
+    country: TCountry;
+    capital: TCity;
+}
+
+export interface IQuizResult extends IQuizAnswer {
+    correct: boolean;
+}
+
+export const getQuizDataRequest = async (): Promise<IQuizData> => {
     const response = await axios.get('http://localhost/api/capital-quiz');
 
-    console.log(response);
-    return response.data
+    return response.data;
+};
+
+export const postQuizAnswer = async ({ country, capital }: IQuizAnswer): Promise<IQuizResult>  => {
+    const response = await axios.post(
+        'http://localhost/api/capital-quiz/answer',
+        {
+            country,
+            capital
+        }
+    );
+
+    return response.data;
 };
